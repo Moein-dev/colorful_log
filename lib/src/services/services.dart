568c9ebd40e1001302638157221e93a93e239303
Moein-dev <flyman812@gamil.com>
2025-02-02 19:04:18 +0330

    fix: improve formatting and readability of extractFileInfo method in DebugLogServices

--- lib/src/services/services.dart
+++ lib/src/services/services.dart
@@ -3,40 +3,42 @@ import 'extract_file_info_service.dart';
 
 class DebugLogServices {
   /// Private method to extract the full file path and line number from stack trace.
-static (String?, String?) extractFileInfo() {
-  StackTrace stackTrace = StackTrace.current;
-  List<String> traceLines = stackTrace.toString().split("\n");
+  static (String?, String?) extractFileInfo() {
+    StackTrace stackTrace = StackTrace.current;
+    List<String> traceLines = stackTrace.toString().split("\n");
 
-  // Detect the first caller OUTSIDE the package
-  String? traceString;
-  for (var line in traceLines) {
-    if (!line.contains('/colorful_log_plus/') && !line.contains('services.dart') && !line.contains('debug_log.dart')) {
-      traceString = line;
-      break;
+    // Detect the first caller OUTSIDE the package
+    String? traceString;
+    for (var line in traceLines) {
+      if (!line.contains('/colorful_log_plus/') &&
+          !line.contains('services.dart') &&
+          !line.contains('debug_log.dart')) {
+        traceString = line;
+        break;
+      }
     }
+
+    if (traceString == null) return (null, null);
+
+    // Extract file path and line number using regex
+    RegExp regex = RegExp(r'\((.*?\.dart):(\d+):\d+\)');
+    Match? match = regex.firstMatch(traceString);
+
+    if (match == null) return (null, null);
+
+    String filePath = match.group(1)!; // Extracted file path
+    String lineNumber = match.group(2)!; // Extracted line number
+
+    // Convert 'package:' paths to absolute paths (only for non-Web)
+    if (!kIsWeb && filePath.startsWith('package:')) {
+      filePath = resolvePackagePath(filePath) ?? filePath;
+    }
+
+    // Web workaround (show only relative package path)
+    if (kIsWeb) {
+      return ("[Web] $filePath:$lineNumber", lineNumber);
+    }
+
+    return ("file://$filePath:$lineNumber", lineNumber);
   }
-
-  if (traceString == null) return (null, null);
-
-  // Extract file path and line number using regex
-  RegExp regex = RegExp(r'\((.*?\.dart):(\d+):\d+\)');
-  Match? match = regex.firstMatch(traceString);
-
-  if (match == null) return (null, null);
-
-  String filePath = match.group(1)!; // Extracted file path
-  String lineNumber = match.group(2)!; // Extracted line number
-
-  // Convert 'package:' paths to absolute paths (only for non-Web)
-  if (!kIsWeb && filePath.startsWith('package:')) {
-    filePath = resolvePackagePath(filePath) ?? filePath;
-  }
-
-  // Web workaround (show only relative package path)
-  if (kIsWeb) {
-    return ("[Web] $filePath:$lineNumber", lineNumber);
-  }
-
-  return ("file://$filePath:$lineNumber", lineNumber);
-}
 }
